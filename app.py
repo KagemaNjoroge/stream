542bb6f1c1d766fe84cd7d787910c3e032c8176c
James Njoroge <93333247+jemo6585@users.noreply.github.com>
2023-05-14 13:15:51 +0300

    remove typo

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PIL import  Image
 col1, col2 = st.columns(spec=[1,1])
-me = Image('images/me.jpg')
-sunday = Image('images/sunday.jpg')
+me = Image.open('images/me.jpg')
+sunday = Image.open('images/sunday.jpg')
 
 with col1:
     st.image(me)
