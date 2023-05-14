e4a950573e6e12a8ecc4f9bbacf256195b366ce2
James Njoroge <93333247+jemo6585@users.noreply.github.com>
2023-05-14 13:15:15 +0300

    Fix Assets rendering

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from PIL import  Image
 col1, col2 = st.columns(spec=[1,1])
+me = Image('images/me.jpg')
+sunday = Image('images/sunday.jpg')
 
 with col1:
-    st.image('images/me.jpg')
+    st.image(me)
 with col2:
-    st.image('images/sunday.jpg')
+    st.image(sunday)
